617dcc28e876e86444f7b1c2a2d96162198fb3d3
ABUBAKAR-ABK <nasirabubakar787@gmail.com>
2024-12-23 23:32:31 +0400

    1st File

---
+++ Practice1.js
@@ -0,0 +1,18 @@
+         // Declare any name as string and print its length 
+    let name = "myname";
+    console.log(name.length);
+
+       // declare your firstname and print its first character 
+    let firstname = "myname";
+    console.log(firstname[0]);
+
+
+    // declare your firstname and print its last character
+    let firstname2 = "myname";
+    console.log(firstname.length-1);
+
+   // output of the code "college"+ 123 
+   // its college123 
+
+   // what are the lengths of an empty string & and a string with a single space 
+   // 1: 0  , 2: 1 
